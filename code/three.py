2c9299c2609b6eda2cd653011b8a3eb2525f93bd
KarthikKrish22 <51352648+KarthikKrish22@users.noreply.github.com>
2019-06-04 00:44:12 -0700

    Create three.py

---
+++ code/three.py
@@ -0,0 +1,7 @@
+charater=input()
+if(charater=='A' or charater=='E' or charater=='I' or charater=='O' or charater=='U' or charater=='a' or charater=='e' or charater=='i' or charater=='o' or charater=='u'):
+    print("Vowel")    
+elif(charater=='B' or charater=='C' or charater=='D'or charater=='F' or charater=='G' or charater=='H' or charater=='J' or charater=='K' or charater=='L' or charater=='M' or charater=='N' or charater=='P' or charater=='Q' or charater=='R' or charater=='S' or charater=='T' or charater=='V' or charater=='W' or charater=='X' or charater=='Y' or charater=='Z' or charater=='b' or charater=='c' or charater=='d'or charater=='f' or charater=='g' or charater=='h' or charater=='j' or charater=='k' or charater=='l' or charater=='m' or charater=='n' or charater=='p' or charater=='q' or charater=='r' or charater=='s' or charater=='t' or charater=='v' or charater=='w' or charater=='x' or charater=='y' or charater=='z'):
+    print("Consonant")
+else:
+    print("invalid")
