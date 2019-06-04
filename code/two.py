7f9c008e08ce2fdfd12959aa64022f744c6a88d0
KarthikKrish22 <51352648+KarthikKrish22@users.noreply.github.com>
2019-06-04 00:23:47 -0700

    Create two.py

---
+++ code/two.py
@@ -0,0 +1,10 @@
+number=int(input())
+if(number>0):
+  if(number%2==0):
+      print("Even")
+  elif(number%2!=0):
+      print("Odd")
+  else:
+      print("invalid")
+else:
+      print("invalid")
